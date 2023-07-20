21f3d2d31abd68f1b5b3f0f3a2623a4fff157901
Shashwat Singh <shashwatsingh741@gmail.com>
2023-07-20 17:13:45 +0530

    Async_Await_Tcp_ClientServer - Server - Added Logic to continuously Accept Client Connections & Read Data on Network Streams

--- Async_Await_Tcp_ClientServer/SocketsServerStarter/ServerSocketAsync/ServerSocket.cs
+++ Async_Await_Tcp_ClientServer/SocketsServerStarter/ServerSocketAsync/ServerSocket.cs
@@ -5,6 +5,8 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace ServerSocketAsync
 {
@@ -13,6 +15,8 @@ namespace ServerSocketAsync
         IPAddress mIP;
         int mPort;
         TcpListener mTcpListener;
+
+        public bool KeepRunning { get; set; }
 
         public async void StartListeningForIncomingConnection(IPAddress iPAddr = null, int iPort = 23000)
         {
@@ -32,12 +36,67 @@ namespace ServerSocketAsync
             System.Diagnostics.Debug.WriteLine(string.Format("IP Address: {0} - Port: {1}", mIP.ToString(), mPort));
 
             mTcpListener = new TcpListener(mIP, mPort);
-            mTcpListener.Start();
 
-            var returnedByAccept = await mTcpListener.AcceptTcpClientAsync();
+            try
+            {
+                mTcpListener.Start();
 
-            System.Diagnostics.Debug.WriteLine("Client Connected successfully: " + returnedByAccept.ToString());
+
+                KeepRunning = true; 
+                while(KeepRunning)
+                {
+                    var returnedByAccept = await mTcpListener.AcceptTcpClientAsync();
+
+                    System.Diagnostics.Debug.WriteLine("Client Connected successfully: " + returnedByAccept.ToString());
+
+                    TakeCareOfTCPClient(returnedByAccept);
+                }
+            }
+
+            catch(Exception excp)
+            {
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
+            
         }
 
+        private async void TakeCareOfTCPClient(TcpClient paramClient)
+        {
+            NetworkStream stream = null;
+            StreamReader reader = null;
+
+            try
+            {
+                stream = paramClient.GetStream();
+                reader = new StreamReader(stream);
+
+                char[] buff = new char[64];
+
+                while (KeepRunning)
+                {
+                    Debug.WriteLine("*** Ready to read");
+
+                    int nRet = await reader.ReadAsync(buff, 0, buff.Length);
+                    // If ReaderAsync returns 0, means the socket is closed & this reads data in char format instead of bytes.
+
+                    if (nRet == 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Socket Disconnected");
+                        break;
+                    }
+
+                    string recievedText = new string(buff);
+
+                    System.Diagnostics.Debug.WriteLine("*** RECIEVED: " + recievedText);
+
+                    Array.Clear(buff, 0, buff.Length);
+                }
+            }
+
+            catch (Exception excp)
+            {
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
+        }
     }
 }
