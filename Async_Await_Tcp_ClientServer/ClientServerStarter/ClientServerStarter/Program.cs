17428fd8cf755dc5c2519e4d2edbd4414ef1d3c8
Shashwat Singh <shashwatsingh741@gmail.com>
2023-07-25 01:00:47 +0530

    Async_Await_Tcp_ClientServer - ClientServerStarter Initialised & Code added to connect to server

---
+++ Async_Await_Tcp_ClientServer/ClientServerStarter/ClientServerStarter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerSocketAsync;
+
+namespace ClientServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientSocket client = new ClientSocket();
+
+            Console.WriteLine("*** Welcome to Socket Client Starter by hzshashwat");
+            Console.WriteLine("Please Type a valid Server IP Address and Press Enter: ");
+
+            string strIPAddress = Console.ReadLine();
+
+            Console.WriteLine("Please Supply a Valid Port Number 0 - 65535 and Press Enter: ");
+            string strPortInput = Console.ReadLine();
+
+
+            if(!client.SetServerIPAddress(strIPAddress) || !client.SetPortNumber(strPortInput))
+            {
+                Console.WriteLine(
+                    string.Format("Wrong IP Address or Port Number supplied - {0} - {1} - Press any key to exit",
+                    strIPAddress,strPortInput));
+                Console.ReadLine();
+                return;
+            }
+
+            Task task = client.ConnectToServer();
+
+            string strInputUser = null;
+
+            do
+            {
+                strInputUser = Console.ReadLine();
+            } while (strInputUser != "<EXIT>");
+        }
+    }
+}
---
+++ Async_Await_Tcp_ClientServer/SocketsServerStarter/ServerSocketAsync/ClientSocket.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSocketAsync
+{
+    public class ClientSocket
+    {
+        IPAddress mServerIPAddress;
+        int mServerPort;
+        TcpClient mClient;
+
+        public ClientSocket()
+        {
+            mClient = null;
+            mServerIPAddress = null;
+            mServerPort = -1;
+        }
+
+        public IPAddress ServerIPAddress
+        {
+            get { return mServerIPAddress; }
+        }
+        
+        public int ServerPort
+        {
+            get { return mServerPort; }
+        }
+
+        public bool SetServerIPAddress(string _IPAddressServer)
+        {
+            IPAddress ipaddr = null;
+
+            if(!IPAddress.TryParse(_IPAddressServer, out ipaddr))
+            {
+                Console.WriteLine("Invalid server IP supplied.");
+                return false;
+            }
+
+            mServerIPAddress = ipaddr;
+
+            return true;
+        }
+
+        public bool SetPortNumber(string _ServerPort)
+        {
+            int portNumber = 0;
+
+            if (!int.TryParse(_ServerPort.Trim(), out portNumber))
+            {
+                Console.WriteLine("Invalid Port number supplied, return.");
+                return false;
+            }
+
+            if (portNumber < 0 || portNumber > 65535)
+            {
+                Console.WriteLine("Port number must be between 0 & 65535");
+                return false;
+            }
+            
+            mServerPort = portNumber;
+            return true;
+        }
+
+        public async Task ConnectToServer()
+        {
+            if(mClient == null)
+            {
+                mClient = new TcpClient();
+            }
+
+            try
+            {
+                await mClient.ConnectAsync(mServerIPAddress, mServerPort);
+                Console.WriteLine(string.Format("Connected to server IP/Port: {0} / {1}",
+                    mServerIPAddress, mServerPort));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
